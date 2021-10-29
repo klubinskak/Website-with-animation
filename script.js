bb31ea70599f831ade4b31cb5a6c4b144ab3a65e
Haraved <76222513+Haraved@users.noreply.github.com>
2021-10-29 11:32:43 +0200

    Add files via upload
    finished with media query

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ let timeline = new TimelineMax();
 
 timeline 
     .fromTo(".bg", {y: -50}, {y:0, duration:5})
+
+
+
+// Hamburger-menu
+
+const hamburger = document.getElementById('hamburger-menu');
+const navRight = document.getElementById('nav-right');
+
+hamburger.addEventListener('click', () => {
+    navRight.classList.toggle('show');
+});
